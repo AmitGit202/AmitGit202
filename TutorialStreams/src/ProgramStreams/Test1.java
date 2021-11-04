2208e0f288d3f701176ee58aaba7654efa472d5f
AmitGit202 <amitava.cse22@gmail.com>
2021-11-04 11:23:05 +0530

    commit existing TCi

--- TutorialStreams/src/ProgramStreams/Test1.java
+++ TutorialStreams/src/ProgramStreams/Test1.java
@@ -34,6 +34,29 @@ public class Test1
 		System.out.println(count);
 
 	}
+//	@Test
+	public void Duplicateregular()
+	{
+		List<String> names = new ArrayList<String>();
+		names.add("Amar");
+		names.add("Raman");
+		names.add("Asit");
+		names.add("Ajay");
+		names.add("Nitu");
+		int count =0;
+
+		for (int i=0; i<names.size();i++)
+		{
+			String actual = names.get(i);
+			if(actual.startsWith("A"))
+			{
+				count++;
+			}
+
+		}
+		System.out.println(count);
+
+	}
 	/* ---------------------------Optimization process---------------------*/
 	//@Test
 	public void streamFilter()
@@ -72,6 +95,9 @@ public class Test1
 		List<String> names = new ArrayList<String>();
 		names.add("Amari");
 		names.add("Raman");
+		names.add("Asit");
+		names.add("Ajay");
+		names.add("Nitu");
 		names.add("Asit");
 		names.add("Ajay");
 		names.add("Nitu");
